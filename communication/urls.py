fd2627051cc3bb53bc27411946fe0eb074ad207a
Junaid Girkar <junaidgirkar@gmail.com>
2020-08-27 14:14:06 +0530

    CRUD Added

--- communication/urls.py
+++ communication/urls.py
@@ -2,10 +2,11 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.create_view, name='create_view'),
-    path('list', views.list_view, name='list_view'),
-    path('<id>', views.detail_view, name='detail_view'),
-    path('<id>/update', views.update_view, name='update_view'),
-    path('<id>/delete', views.delete_view, name='delete_view'),
+    path('', views.create_view.as_view(), name='create_view'),
+    path('list', views.list_view.as_view(), name='list_view'),
+    path('<id>', views.detail_view.as_view(), name='detail_view'),
+    path('<id>/update', views.update_view.as_view(), name='update_view'),
+    path('<id>/delete', views.delete_view.as_view(), name='delete_view'),
+    path('result', views.result_view, name='result_view'),
 
 ]
--- communication/views.py
+++ communication/views.py
@@ -1,57 +1,98 @@
 from django.shortcuts import render, HttpResponseRedirect, get_object_or_404
 from .models import Quiz
 from .forms import QuizForm
-
+from django.views import View
 # Create your views here.
 
-def create_view(request):
-    context = {}
+class create_view(View):
+    def get(self, request):
+        context = {}
 
-    form = QuizForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-    context['form'] = form
-    return render(request, 'communication\\create_view.html', context)
+        form = QuizForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+        context['form'] = form
+        return render(request, 'communication\\create_view.html', context)
+    def post(self,request):
+        context = {}
 
-def list_view(request):
-    context = {}
-    context['dataset'] = Quiz.objects.all()
+        form = QuizForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+        context['form'] = form
+        return render(request, 'communication\\create_view.html', context)
 
-    return render(request, 'communication\\list_view.html', context)
 
-def detail_view(request, id):
-    context = {}
-    context["data"] = Quiz.objects.get(id = id)
+class list_view(View):
+    def get(self, request):
+        context = {}
+        context['dataset'] = Quiz.objects.all()
 
-    return render(request, 'communication\\detail_view.html', context)
+        return render(request, 'communication\\list_view.html', context)
 
-def update_view(request, id):
-    context = {}
-    # fetch object related to passed it
-    obj = get_object_or_404(Quiz, id=id)
+class detail_view(View):
+    def get(self, request, id):
+        context = {}
+        context["data"] = Quiz.objects.get(id = id)
 
-    #pass the obj as instance in form
-    form = QuizForm(request.POST or None, instance = obj)
+        return render(request, 'communication\\detail_view.html', context)
 
-    #Save data and redirect
-    if form.is_valid():
-        form.save()
-        return HttpResponseRedirect("/"+id)
+class update_view(View):
+    def get(self, request, id):
+        context = {}
+        # fetch object related to passed it
+        obj = get_object_or_404(Quiz, id=id)
+        #pass the obj as instance in form
+        form = QuizForm(request.POST or None, instance = obj)
+        #Save data and redirect
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("/"+id)
+        #add form directry to context
+        context['form'] = form
+        return render(request, 'communication\\update_view.html', context)
 
-    #add form directry to context
-    context['form'] = form
+    def post(self, request, id):
+        context = {}
+        # fetch object related to passed it
+        obj = get_object_or_404(Quiz, id=id)
+        # pass the obj as instance in form
+        form = QuizForm(request.POST or None, instance=obj)
+        # Save data and redirect
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("/" + id)
+        # add form directry to context
+        context['form'] = form
+        return render(request, 'communication\\update_view.html', context)
 
-    return render(request, 'communication\\update_view.html', context)
+class delete_view(View):
+    def get(self, request, id):
+        context = {}
+        #fetch the obj related to passed id
+        obj = get_object_or_404(Quiz, id = id)
 
-def delete_view(request, id):
-    context = {}
-    #fetch the obj related to passed id
-    obj = get_object_or_404(Quiz, id = id)
+        if request.method == 'POST':
+            # Delete Object
+            obj.delete()
+            # After Deleting Redirect to Home Page
+            return HttpResponseRedirect('/')
+        return render(request, "communication\\delete_view.html", context)
 
-    if request.method == 'POST':
-        # Delete Object
-        obj.delete()
-        # After Deleting Redirect to Home Page
-        return HttpResponseRedirect('/')
-    return render(request, "communication\\delete_view.html", context)
+    def post(self, request, id):
+        context = {}
+        # fetch the obj related to passed id
+        obj = get_object_or_404(Quiz, id=id)
 
+        if request.method == 'POST':
+            # Delete Object
+            obj.delete()
+            # After Deleting Redirect to Home Page
+            return HttpResponseRedirect('/')
+        return render(request, "communication\\delete_view.html", context)
+
+
+def result_view(request):
+    score = 0
+    args = {score: score}
+    return render(request, 'communication\\result_view.html', args)
